89a0556878645f5fa0bc0ef51b172aa5f630e0bb
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2026-02-19 22:57:40 -0500

    bro..

--- mrchromebox.sh
+++ mrchromebox.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 #skiddy mrchromebox for miniOS script
+get_fixed_dst_drive() {
+	local dev
+	if [ -z "${DEFAULT_ROOTDEV}" ]; then
+		for dev in /sys/block/sd* /sys/block/mmcblk*; do
+			if [ ! -d "${dev}" ] || [ "$(cat "${dev}/removable")" = 1 ] || [ "$(cat "${dev}/size")" -lt 2097152 ]; then
+				continue
+			fi
+			if [ -f "${dev}/device/type" ]; then
+				case "$(cat "${dev}/device/type")" in
+				SD*)
+					continue;
+					;;
+				esac
+			fi
+			DEFAULT_ROOTDEV="{$dev}"
+		done
+	fi
+	if [ -z "${DEFAULT_ROOTDEV}" ]; then
+		dev=""
+	else
+		dev="/dev/$(basename ${DEFAULT_ROOTDEV})"
+		if [ ! -b "${dev}" ]; then
+			dev=""
+		fi
+	fi
+	echo "${dev}"
+}
+intdis=$(get_fixed_dst_drive)
+if echo "$intdis" | grep -q '[0-9]$'; then
+	intdis_prefix="$intdis"p
+else
+	intdis_prefix="$intdis"
+fi
 cd /
-mount /dev/mmcblk0p3 /usb -o ro || mount /dev/mmcblk1p3 /usb -o ro
+mount "$intdis_prefix"3 /usb -o ro || mount "$intdis_prefix"5 /usb -o ro
 mount --bind /dev /usb/dev
 mount --bind /proc /usb/proc
 mount --bind /sys /usb/sys
